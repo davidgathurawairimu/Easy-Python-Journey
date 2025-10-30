605abc575fa1a6c3e9471081c9d358a8d2f1ca4d
davidgathurawairimu <wairimudavidgathura@gmail.com>
2025-10-30 17:50:34 +0300

    Delete Advanced directory

--- Advanced/522-except.py
+++
@@ -1,8 +0,0 @@
-figuratives = ['Simile','Metaphor','Personification','Hyperbole','Allusion']
-try:
-    figurative_input = int(input('Enter a number from 1-5 to get an example '))
-    figurative = figuratives[figurative_input-1]
-except:
-    print('You did not enter a figurative. Try again')
-
-
--- Advanced/525-raise.py
+++
@@ -1,5 +0,0 @@
-temperature = float(input("What temperature is absolute zero "))
-if temperature > 0:
-    raise ValueError("The temperature has to be negative")
-else:
-    print("The temperature is negative, yes, -459.67 F to be exact.")
